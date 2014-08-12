103481332e993ad5e4c6cbb23ae20eeebfb3ba6f
Dave Smith <smith@wiresareobsolete.com>
2014-08-12 12:20:53 -0600

    With AsyncTask for posting.

--- app/src/main/java/com/hp/android/yamba/StatusActivity.java
+++ app/src/main/java/com/hp/android/yamba/StatusActivity.java
@@ -1,17 +1,28 @@
 package com.hp.android.yamba;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClientException;
 
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements TextWatcher {
+
+    private static final String TAG = StatusActivity.class.getSimpleName();
+
+    private static final int MAX_CHARS = 140;
 
     private TextView mStatusCounter;
     private EditText mStatusText;
@@ -25,10 +36,68 @@ public class StatusActivity extends Activity {
         mStatusCounter = (TextView) findViewById(R.id.status_counter);
         mStatusText = (EditText) findViewById(R.id.status_text);
         mStatusButton = (Button) findViewById(R.id.status_button);
+
+        mStatusText.addTextChangedListener(this);
+        if (savedInstanceState == null) {
+            mStatusText.setText(null);
+        }
     }
 
     public void onPostClick(View v) {
-        Toast.makeText(this, "Click Works!!", Toast.LENGTH_SHORT).show();
+        final String status = mStatusText.getText().toString();
+        Log.d(TAG, "Posting Status...");
+
+        StatusUpdateTask task = new StatusUpdateTask();
+        task.execute(status);
+    }
+
+    private class StatusUpdateTask extends AsyncTask<String, Void, Boolean> {
+
+        private ProgressDialog mProgress;
+
+        @Override
+        protected void onPreExecute() {
+            mProgress = ProgressDialog.show(StatusActivity.this,
+                    "Posting...", "Posting Status", true);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            final String status = params[0];
+            YambaClient client = new YambaClient("student", "password");
+
+            try {
+                client.postStatus(status);
+                Log.d(TAG, "Status Complete");
+            } catch (YambaClientException e) {
+                Log.wtf(TAG, "Error Posting Status", e);
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            mProgress.dismiss();
+            if (result) {
+                mStatusText.getText().clear();
+            }
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        int remaining = MAX_CHARS - s.length();
+        mStatusCounter.setText(String.valueOf(remaining));
+
+        mStatusButton.setEnabled(remaining >= 0);
     }
 
     @Override
