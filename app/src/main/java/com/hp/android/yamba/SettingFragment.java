2372749185c99440166a203b459e07fae9e31c38
Dave Smith <smith@wiresareobsolete.com>
2014-08-12 15:43:14 -0600

    Added preferences.

---
+++ app/src/main/java/com/hp/android/yamba/SettingFragment.java
@@ -0,0 +1,16 @@
+package com.hp.android.yamba;
+
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+
+public class SettingFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
---
+++ app/src/main/java/com/hp/android/yamba/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.hp.android.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
